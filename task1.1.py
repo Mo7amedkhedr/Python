0216a6a4c7f111774402e9a383b5e24e8b7d95db
Mo7amedkhedr <khedr5322@gmail.com>
2024-06-14 15:36:32 +0300

    task count number 4 in list

---
+++ task1.1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+my_list = [4,1,2,4,3,4,6,4,8,4]
+
+'''
+count=0
+for i in my_list:
+    if i == 4:
+        count= count+1
+print(f"the number of times a number_4 is repeated = {count} ")'''
+
+print(my_list.count(4))
+
+
+
