0a23392da404a8b6ed07ef133abae640f5f425d3
Mo7amedkhedr <khedr5322@gmail.com>
2024-06-14 17:17:47 +0300

    task calculation

---
+++ lecture 1/task1.5.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def sum(num1, num2):
+    print(f"the sum = {num1+num2}")
+
+def sub(num1, num2):
+    print(f"the sub = {num1-num2}")
+
+def mul(num1, num2):
+    print(f"the mul = {num1*num2}")
+
+def div(num1, num2):
+    print(f"the sum = {num1/num2}")    
+
+sum(5,2)
+sub(6,5)
+mul(10,6)
+div(9,3)
