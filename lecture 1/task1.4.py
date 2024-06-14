495762a0d26fffce2813108fb404c40f0103e4e6
Mo7amedkhedr <khedr5322@gmail.com>
2024-06-14 17:12:21 +0300

    task information about me

---
+++ lecture 1/task1.4.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def info (name, address, email, faculty, birth_year):
+    print("My name is: ",name)
+    print("My birth Year: ",birth_year)
+    print("My address: ",address)
+    print("My email: ",email)
+    print("My faculty: ",faculty)
+
+info("mohamed khedr","EL-Mansoura","khedr8538@gmail.com","Faculty of engineering",2002)
